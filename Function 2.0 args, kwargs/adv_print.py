a9ab015d9535cca51ba79457c1722236637e6844
Pavel <it.pavelkozlov@gmail.com>
2019-03-10 20:56:08 +0300

    Added many params in adv_print"

--- Function 2.0 args, kwargs/adv_print.py
+++ Function 2.0 args, kwargs/adv_print.py
@@ -1,8 +1,15 @@
-def adv_print(text, start=0, **kwargs):
-    result = str(text)[start:]
+def adv_print(*args, start=0, **kwargs):
+    args_list = list()
+    for item in args:
+        args_list.append(str(item))
+    result = ' '.join(args_list)
+
+    if start > 0:
+        result = result[start:]
+
     if kwargs.get('max_line'):
-        res = ''
         counter = 0
+        res = ''
         for symbol in result:
             if counter < kwargs['max_line']:
                 res += symbol
@@ -12,13 +19,11 @@ def adv_print(text, start=0, **kwargs):
                 counter = 0
             counter += 1
         result = res
+        del kwargs['max_line']
+
     if kwargs.get('in_file'):
-        with open(f'{text}.txt', 'w') as file:
+        with open('text_file.txt', 'w') as file:
             file.write(result)
-    print(result)
-    return result
+        del kwargs['in_file']
 
-
-adv_print('I Love Netology', max_line=4, in_file=True)
-adv_print('Python')
-adv_print(139595, start=2)
+    print(*result.split(' '), **kwargs)
